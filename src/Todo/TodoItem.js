53d4e128d240486ec94c7b0518e283d1c6864f6d
Aleksandr <mishenko.aleksandr@gmail.com>
2020-02-13 21:40:55 +0200

    marked out the structure of TodoItem and add css

--- src/Todo/TodoItem.js
+++ src/Todo/TodoItem.js
@@ -1,11 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./todo.css";
 
 function TodoItem({ todo, index }) {
   return (
-    <li>
-      <strong>{index + 1}</strong>
-      {todo.title}
+    <li className="li">
+      <span>
+        <input type="checkbox" style={{ marginRight: "1rem" }} />
+        <strong>{index + 1}</strong>
+        &nbsp;
+        {todo.title}
+      </span>
+
+      <button className="rm">&times;</button>
     </li>
   );
 }
