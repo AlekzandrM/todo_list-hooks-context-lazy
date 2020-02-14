66d5a9aa6206cb4b901b7599b45f976c0dd3d149
Aleksandr <mishenko.aleksandr@gmail.com>
2020-02-14 17:46:07 +0200

    loaded todos from jsonplaceholder

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import TodoList from "./Todo/TodoList";
 import Context from "./context";
 import AddTodo from "./Todo/AddTodo";
 
 function App() {
-  let [todos, setTodos] = React.useState([
-    { id: 1, completed: false, title: "To buy bread" },
-    { id: 2, completed: true, title: "To buy milk" },
-    { id: 3, completed: false, title: "To buy butter" }
-  ]);
+  const [todos, setTodos] = React.useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos?_limit=5")
+      .then((response) => response.json())
+      .then((todos) => {
+        setTodos(todos);
+      });
+  }, []);
 
   function handleToggle(id) {
     setTodos(
