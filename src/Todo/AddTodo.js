723a630e9b4758d92d820d7d51da1e20b54f8381
Aleksandr <mishenko.aleksandr@gmail.com>
2020-02-14 14:58:08 +0200

    Created own hook useInputHook and add PropTypes in AddTodo

--- src/Todo/AddTodo.js
+++ src/Todo/AddTodo.js
@@ -1,26 +1,40 @@
 import React from "react";
+import PropTypes from "prop-types";
+
+function useInputHook(defaultValue = "") {
+  const [value, setValue] = React.useState("");
+  return {
+    bind: {
+      value: value,
+      onChange: (e) => setValue(e.target.value)
+    },
+    clear: () => setValue(""),
+    value: () => value
+  };
+}
 
 function AddTodo({ onCreate }) {
-  const [value, setValue] = React.useState("");
+  const input = useInputHook("");
 
   function handleSubmit(e) {
     e.preventDefault();
-    if (value.trim()) {
-      onCreate(value);
-      setValue("");
+
+    if (input.value().trim()) {
+      onCreate(input.value());
+      input.clear();
     }
   }
 
   return (
     <form style={{ marginBottom: "1rem" }} onSubmit={handleSubmit}>
-      <input
-        type="text"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-      />
+      <input type="text" {...input.bind} />
       <button type="submit">Add toto</button>
     </form>
   );
 }
+
+AddTodo.propTypes = {
+  onCreate: PropTypes.func.isRequired
+};
 
 export default AddTodo;
